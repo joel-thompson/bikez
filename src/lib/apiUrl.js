31421dee731d7d65f58d694fd7a9977737024484
Joel Thompson <joel.thompson@intercom.io>
2020-05-22 15:12:03 -0700

    fix

--- src/lib/apiUrl.js
+++ src/lib/apiUrl.js
@@ -4,7 +4,7 @@ function apiUrl(url) {
     baseUrl = "http://localhost:2000"
   }
   else {
-    baseUrl = "https://api.bikenumbers.dev/"
+    baseUrl = "https://api.bikenumbers.dev"
   }
   return `${baseUrl}/${url}`
 }

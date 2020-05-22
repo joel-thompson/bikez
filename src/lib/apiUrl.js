088a0f7ecef0f65b4faae3a7d540860fcf184b65
Joel Thompson <joel.thompson@intercom.io>
2020-05-22 07:42:24 -0700

    fix api url

--- src/lib/apiUrl.js
+++ src/lib/apiUrl.js
@@ -4,7 +4,7 @@ function apiUrl(url) {
     baseUrl = "http://localhost:2000"
   }
   else {
-    baseUrl = "https://bikez.herokuapp.com"
+    baseUrl = "https://bikez-api.herokuapp.com"
   }
   return `${baseUrl}/${url}`
 }

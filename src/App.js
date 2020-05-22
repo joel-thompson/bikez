5c64180c61b0da082a6960cf326ab471c39b6883
Joel Thompson <joel.thompson@intercom.io>
2020-05-21 19:25:29 -0700

    less dupe

--- src/App.js
+++ src/App.js
@@ -27,11 +27,9 @@ function App() {
     async function checkLoginStatus() {
       await axios.get("http://localhost:2000/logged_in", { withCredentials: true }).then(response => {
         if (response.data.logged_in && loggedInStatus === "NOT_LOGGED_IN") {
-          setLoggedInStatus("LOGGED_IN")
-          setUser(response.data.user)
+          handleLogin(response.data)
         } else if (!response.data.logged_in && loggedInStatus === "LOGGED_IN") {
-          setLoggedInStatus("NOT_LOGGED_IN")
-          setUser({})
+          handleLogout() 
         }
       }).catch(error => {
         console.log("check login error", error)

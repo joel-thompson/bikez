0a1e89ea5206737121da74e28b5bae8c4aea99ee
Joel Thompson <joel.thompson@intercom.io>
2020-05-23 12:58:38 -0700

    set up prettier and block rendering until app is ready

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,30 +1,36 @@
-import React, { useState } from "react";
-import axios from "axios";
-import apiUrl from "../../lib/apiUrl"
+import React, { useState } from 'react';
+import axios from 'axios';
+import apiUrl from '../../lib/apiUrl';
 
 function Login(props) {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [loginErrors, setLoginErrors] = useState(null)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginErrors, setLoginErrors] = useState(null);
 
   function handleSubmit(event) {
-    event.preventDefault()
-    axios.post(apiUrl("sessions"), {
-      user: {
-        email: email,
-        password: password,
-      }
-    },
-    { withCredentials: true }).then(response => {
-      if (response.data.logged_in) {
-        props.handleSuccessfulAuth(response.data)
-      } else {
-        setLoginErrors("Unable to login")
-      }
-      console.log("login response", response)
-    }).catch(error => {
-      console.log("login error", error)
-    })
+    event.preventDefault();
+    axios
+      .post(
+        apiUrl('sessions'),
+        {
+          user: {
+            email: email,
+            password: password,
+          },
+        },
+        { withCredentials: true },
+      )
+      .then((response) => {
+        if (response.data.logged_in) {
+          props.handleSuccessfulAuth(response.data);
+        } else {
+          setLoginErrors('Unable to login');
+        }
+        console.log('login response', response);
+      })
+      .catch((error) => {
+        console.log('login error', error);
+      });
   }
 
   return (
@@ -35,23 +41,23 @@ function Login(props) {
         </div>
       )}
       <form onSubmit={handleSubmit}>
-         <input 
-            type="email"
-            placeholder="Email"
-            required
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
 
-         <input 
-            type="password"
-            placeholder="Password"
-            required
-            value={password}
-            onChange={event => setPassword(event.target.value)}
-          />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
 
-          <button type="submit">Login</button>
+        <button type="submit">Login</button>
       </form>
     </div>
   );
--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -1,31 +1,39 @@
-import React, { useState } from "react";
-import axios from "axios";
-import apiUrl from "../../lib/apiUrl"
+import React, { useState } from 'react';
+import axios from 'axios';
+import apiUrl from '../../lib/apiUrl';
 
 function Registration(props) {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [passwordConfirmation, setPasswordConfirmation] = useState("")
-  const [registrationErrors, setRegistrationErrors] = useState(null)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState(
+    '',
+  );
+  const [registrationErrors, setRegistrationErrors] = useState(null);
 
   function handleSubmit(event) {
-    event.preventDefault()
-    axios.post(apiUrl("registrations"), {
-      user: {
-        email: email,
-        password: password,
-        password_confirmation: passwordConfirmation,
-      }
-    },
-    { withCredentials: true }).then(response => {
-      if (response.data.status === "created") {
-        props.handleSuccessfulAuth(response.data)
-      } else {
-        setRegistrationErrors("Unable to register")
-      }
-    }).catch(error => {
-      console.log("registration error", error)
-    })
+    event.preventDefault();
+    axios
+      .post(
+        apiUrl('registrations'),
+        {
+          user: {
+            email: email,
+            password: password,
+            password_confirmation: passwordConfirmation,
+          },
+        },
+        { withCredentials: true },
+      )
+      .then((response) => {
+        if (response.data.status === 'created') {
+          props.handleSuccessfulAuth(response.data);
+        } else {
+          setRegistrationErrors('Unable to register');
+        }
+      })
+      .catch((error) => {
+        console.log('registration error', error);
+      });
   }
 
   return (
@@ -36,31 +44,33 @@ function Registration(props) {
         </div>
       )}
       <form onSubmit={handleSubmit}>
-         <input 
-            type="email"
-            placeholder="Email"
-            required
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
 
-         <input 
-            type="password"
-            placeholder="Password"
-            required
-            value={password}
-            onChange={event => setPassword(event.target.value)}
-          />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
 
-         <input 
-            type="password"
-            placeholder="Password Confirmation"
-            required
-            value={passwordConfirmation}
-            onChange={event => setPasswordConfirmation(event.target.value)}
-          />
+        <input
+          type="password"
+          placeholder="Password Confirmation"
+          required
+          value={passwordConfirmation}
+          onChange={(event) =>
+            setPasswordConfirmation(event.target.value)
+          }
+        />
 
-          <button type="submit">Register</button>
+        <button type="submit">Register</button>
       </form>
     </div>
   );
